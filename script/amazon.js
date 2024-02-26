9c979dfc09304b857ef516a0276ea7eaf9d63337
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-02-26 10:46:21 +0800

    Fixed the quantity cart

--- script/amazon.js
+++ script/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -64,6 +64,7 @@ document.querySelectorAll('.js-add-to-cart')
       let productId = button.dataset.productId;
 
       let matchingItem;
+      let itemQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
       cart.forEach((item) => {
         if(productId === item.productId){
@@ -72,11 +73,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if(matchingItem){
-        matchingItem.quantity += 1;
+        matchingItem.quantity += itemQuantity;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: itemQuantity
         });
       }
 
