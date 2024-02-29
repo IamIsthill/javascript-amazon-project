600dcb414eb8e3c5283c8c71f4b3398dd800ff48
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-02-29 18:13:41 +0800

    fixed radio selectors

--- script/amazon.js
+++ script/amazon.js
@@ -1,6 +1,6 @@
-import {cart} from '../data/cart.js';
-import {products, addToCart} from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productHTML = '';
 
