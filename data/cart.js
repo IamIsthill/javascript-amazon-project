c40e0ee8c0730536274fbaff3345cfe416a76011
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-03-01 22:17:22 +0800

    Found the bug

--- data/cart.js
+++ data/cart.js
@@ -21,7 +21,7 @@ export function addToCart(productId){
   let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
   cart.forEach((cartItem) => {
-    if(productId === cartItem.id){
+    if(productId === cartItem.productId){
       matchingItem = cartItem;
     }
   });
@@ -72,7 +72,7 @@ export function updateQuantity(productId, newQuantity) {
     }
   });
 
-  matchingItem.quantity = newQuantity;
+  cartItem.quantity = newQuantity;
 
   saveToStorage();
 }
