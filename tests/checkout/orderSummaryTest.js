ad4dde184bf2f73d0ac643ce90cebf759f03d659
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-03-03 18:21:53 +0800

    Test if the product price remains the same

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,8 +1,10 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
+
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
     document.querySelector('.js-test-container').innerHTML = `
@@ -23,9 +25,11 @@ describe('test suite: renderOrderSummary', () => {
     loadFromStorage();
     renderOrderSummary();
   });
+
   afterEach(() => {
     document.querySelector('.js-test-container').innerHTML = '';
   });
+
   it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
@@ -42,6 +46,12 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('$10.90');
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('$20.95');
   });
 
   it('removes a product', () => {
@@ -58,6 +68,9 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('$20.95');
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
   });
